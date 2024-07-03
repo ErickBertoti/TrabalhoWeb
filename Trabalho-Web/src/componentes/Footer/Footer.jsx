e649703fb5c9c58567c15fb6934a65642fd0a055
DiogoDosSantosMarques <marques.gogosantos@gmail.com>
2024-07-03 13:35:01 -0300

    contato no footer adaptado

--- Trabalho-Web/src/componentes/Footer/Footer.jsx
+++ Trabalho-Web/src/componentes/Footer/Footer.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './Footer.css';
 
 const Footer = () => {
+  const [contactEmail, setContactEmail] = useState('');
+  const [contactPhone, setContactPhone] = useState('');
+
+  useEffect(() => {
+    // Recuperar dados do Local Storage
+    const savedContact = localStorage.getItem('contact');
+    if (savedContact) {
+      const contactData = JSON.parse(savedContact);
+      setContactEmail(contactData.email);
+      setContactPhone(contactData.telefone);
+    }
+  }, []);
+
   return (
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-email">
           <p>Entre em contato via email:</p>
-          <a href="mailto:contato@exemplo.com">contato@exemplo.com</a>
+          <a href={`mailto:${contactEmail}`}>{contactEmail || 'contato@exemplo.com'}</a>
         </div>
         <div className="footer-whatsapp">
           <p>Entre em contato via WhatsApp:</p>
-          <a href="https://api.whatsapp.com/send?phone=55123456789" target="_blank" rel="noopener noreferrer">12 3456-7890</a>
+          <a href={`https://api.whatsapp.com/send?phone=${contactPhone}`} target="_blank" rel="noopener noreferrer">
+            {contactPhone || '12 3456-7890'}
+          </a>
         </div>
       </div>
     </footer>
