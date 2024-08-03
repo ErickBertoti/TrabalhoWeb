0602d57195c70002dbde41213738bf972b8cc969
ErickBertoti <erickhoffmann2301@gmail.com>
2024-08-02 21:24:03 -0300

    Footer Corrigido

--- Trabalho-Web/src/componentes/Footer/Footer.jsx
+++ Trabalho-Web/src/componentes/Footer/Footer.jsx
@@ -2,19 +2,7 @@ import { useEffect, useState } from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  const [contactEmail, setContactEmail] = useState('');
-  const [contactPhone, setContactPhone] = useState('');
-
-  useEffect(() => {
-    // Recuperar dados do Local Storage
-    const savedContact = localStorage.getItem('contact');
-    if (savedContact) {
-      const contactData = JSON.parse(savedContact);
-      setContactEmail(contactData.email);
-      setContactPhone(contactData.telefone);
-    }
-  }, []);
-
+ 
   return (
     <footer className="footer">
       <div className="footer-content">
